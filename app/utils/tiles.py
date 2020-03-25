d90f74abb198462cfa30ed9769d498e2f0e58ba4
Thomas Maschler <tmaschler@wri.org>
2020-03-25 19:53:23 -0400

    compute bbox for tile in WM

--- app/utils/tiles.py
+++ app/utils/tiles.py
@@ -4,6 +4,6 @@ from shapely.geometry import box
 
 def to_bbox(x: int, y: int, z: int) -> box:
     tile = mercantile.Tile(x, y, z)
-    left, bottom, right, top = mercantile.bounds(tile)
+    left, bottom, right, top = mercantile.xy_bounds(tile)
 
     return box(left, bottom, right, top)
