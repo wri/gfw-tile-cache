c07ef1a2272964d9026d55951a98da1d60e64627
Thomas Maschler <tmaschler@wri.org>
2020-03-30 10:37:19 -0400

    fix response data

--- app/services/features/geostore.py
+++ app/services/features/geostore.py
@@ -27,5 +27,5 @@ async def get_geostore(dataset, version, geostore_id):
 
     async with pool.acquire() as conn:
         sql = await conn.prepare(str(sql))
-        max_date = await sql.fetchval(timeout=30)
-    return {"max_date": max_date}
+        geostore = await sql.fetchone(timeout=30)
+    return geostore
