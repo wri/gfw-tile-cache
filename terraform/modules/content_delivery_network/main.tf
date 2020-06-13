27f99c0d695600ed808c59ddc690930ab15fbc47
Thomas Maschler <tmaschler@wri.org>
2020-06-12 23:15:10 -0400

    remove archive file for deleted lambda function

--- terraform/modules/content_delivery_network/main.tf
+++ terraform/modules/content_delivery_network/main.tf
@@ -228,12 +228,6 @@ data "archive_file" "redirect_latest_tile_cache" {
   output_path = "${path.module}/lambda_functions/redirect_latest_tile_cache.zip"
 }
 
-data "archive_file" "reset_response_header_caching" {
-  type        = "zip"
-  source_dir  = "${path.module}/lambda_functions/reset_response_header_caching/src"
-  output_path = "${path.module}/lambda_functions/reset_response_header_caching.zip"
-}
-
 data "archive_file" "redirect_s3_404" {
   type        = "zip"
   source_dir  = "${path.module}/lambda_functions/redirect_s3_404/src"
