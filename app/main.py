16c481a5be5e13e9a445d87a287cecaa91d13696
Thomas Maschler <tmaschler@wri.org>
2021-03-05 17:41:39 -0500

    add wmts router

--- app/main.py
+++ app/main.py
@@ -32,6 +32,7 @@ from .routes.nasa_viirs_fire_alerts import vector_tiles as viirs_vector_tiles
 from .routes.umd_tree_cover_loss import raster_tiles as umd_tree_cover_loss_raster_tiles
 from .routes.umd_glad_alerts import raster_tiles as umd_glad_alerts_raster_tiles
 from .routes.wur_radd_alerts import raster_tiles as wur_radd_alerts_raster_tiles
+from .routes import wmts
 
 gunicorn_logger = logging.getLogger("gunicorn.error")
 logger.handlers = gunicorn_logger.handlers
@@ -49,6 +50,7 @@ ROUTERS = (
     umd_glad_alerts_raster_tiles.router,
     wur_radd_alerts_raster_tiles.router,
     raster_tiles.router,
+    wmts.router,
     viirs_esri_vector_tile_server.router,
     esri_vector_tile_server.router,
     helpers.router,
