ae2ddd17c277beed1661097fd146961c52bbf3f6
justin.terry@wri.org <jt@c02xrkdcjg5h.myfiosgateway.com>
2021-06-14 16:53:32 -0400

    Drop schema on test db session end

--- tests/fixtures/session_end.sql
+++ tests/fixtures/session_end.sql
@@ -4,4 +4,4 @@ DROP TABLE IF EXISTS  public.versions CASCADE;
 DROP TABLE IF EXISTS  public.assets CASCADE;
 DROP TABLE IF EXISTS  public.geostore CASCADE;
 DROP TABLE IF EXISTS  umd_modis_burned_areas.v202003 CASCADE;
-
+DROP SCHEMA IF EXISTS umd_modis_burned_areas;
