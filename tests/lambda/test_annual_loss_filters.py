d86a9eb15e6f3f28ad6c7576cb24a037b5ecd534
Gary Tempus <gtempus@users.noreply.github.com>
2022-08-17 14:19:17 -0400

    GTC-1876 Dynamic TCL Tile Requests Not Responding (#106)
    * chore: Update black package in pre-commit
    
    * chore: Ignore generated terraform files
    A newer version of terraform is installed by Docker. During terraform execution, additional files are being created.
    
    * fix: Ignore band 4 from Tree Cover Loss dataset
    Tree Cover Loss (TCL) versions >= `v1.9` [have added an alpha channel](https://github.com/wri/gfw-data-api/commit/d3c32ac8f9378bdcd50584b020e02d557860cbef) to its year_intensity symbology. However, the annual loss filter wasn't updated in the lambda function. So, the lambda would fail attempting to unpack the bands (it assumed only 3 bands).
    
    * refactor: Create test data in separate method
    
    * refactor: Increase readability of annual loss filter tests
    Attempt to show the relationships between zoom levels better than just asserting literals.
    
    * refactor: Increase readability of annual loss filter tests
    Attempt to show the relationships between zoom levels better than just asserting literals.
    
    * refactor: Use 'magnitude' instead of 'intensity' for rgb
    
    GTC-1876
    
    Co-authored-by: Thomas Maschler <tmaschler@wri.org>
    Co-authored-by: jterry64 <justin.terry@wri.org>
    Co-authored-by: Solomon Negusse <solomon.negusse@wri.org>
    Co-authored-by: Daniel Mannarino <daniel.mannarino@gmail.com>

--- tests/lambda/test_annual_loss_filters.py
+++ tests/lambda/test_annual_loss_filters.py
@@ -7,7 +7,6 @@ from lambdas.raster_tiler.lambda_function import (
 
 
 def test_scale_intensity():
-
     scale_zoom_12 = scale_intensity(12)
     assert scale_zoom_12(0) == 0
     assert scale_zoom_12(50) == 50
@@ -27,61 +26,122 @@ def test_scale_intensity():
     assert scale_zoom_2(255) == 255
 
 
-def test_apply_annual_loss_filter():
-    # Create test data
-
+def _create_tcl_3band_data():
     intensity = (np.ones((4, 3)) * 100).astype("uint8")
+    all_zeros = (np.zeros((4, 3))).astype("uint8")
     year_2000 = (np.zeros((1, 3))).astype("uint8")
     year_2005 = (np.ones((1, 3)) * 5).astype("uint8")
     year_2010 = (np.ones((1, 3)) * 10).astype("uint8")
     year_2015 = (np.ones((1, 3)) * 15).astype("uint8")
     years = np.vstack([year_2000, year_2005, year_2010, year_2015])
-    input_data = np.array([intensity, intensity, years])
+    return np.array([intensity, all_zeros, years])
 
-    result = apply_annual_loss_filter(
-        input_data, z="12", start_year="2000", end_year="2020"
-    )
-    red, green, blue, alpha = result
-    assert np.all(red == 228)
-    assert np.all(green == 137)
-    assert np.all(blue == 165)
-    assert np.all(alpha[0] == 0)
-    assert np.all(alpha[1] == 100)
-    assert np.all(alpha[2] == 100)
-    assert np.all(alpha[3] == 100)
 
-    result = apply_annual_loss_filter(
-        input_data, z="12", start_year="2006", end_year="2014"
-    )
-    red, green, blue, alpha = result
-    assert np.all(red == 228)
-    assert np.all(green == 137)
-    assert np.all(blue == 165)
-    assert np.all(alpha[0] == 0)
-    assert np.all(alpha[1] == 0)
-    assert np.all(alpha[2] == 100)
-    assert np.all(alpha[3] == 0)
+def _create_tcl_4band_data():
+    """
+    This creates Tree Cover Loss (TCL) test data >= v1.9.
+    In this version, a 4th band is created as an 'alpha' channel for year_intensity symbology
+    https://github.com/wri/gfw-data-api/commit/d3c32ac8f9378bdcd50584b020e02d557860cbef
+    """
+    tcl_3band_data = _create_tcl_3band_data()
+    tcl_4th_band = (np.ones((4, 3)) * 255).astype("uint8")
+    return np.append(tcl_3band_data, [tcl_4th_band], axis=0)
 
-    result = apply_annual_loss_filter(
-        input_data, z="12", start_year=None, end_year=None
-    )
-    red, green, blue, alpha = result
-    assert np.all(red == 228)
-    assert np.all(green == 137)
-    assert np.all(blue == 165)
-    assert np.all(alpha[0] == 0)
-    assert np.all(alpha[1] == 100)
-    assert np.all(alpha[2] == 100)
-    assert np.all(alpha[3] == 100)
 
-    result = apply_annual_loss_filter(
-        input_data, z="10", start_year="2001", end_year="2020"
+def _apply_filter_with(data, zoom_level="10", start_year=None, end_year=None):
+    return apply_annual_loss_filter(
+        data, z=zoom_level, start_year=start_year, end_year=end_year
     )
-    red, green, blue, alpha = result
-    assert np.all(red == 228)
-    assert np.all(green == 122)
-    assert np.all(blue == 166)
-    assert np.all(alpha[0] == 0)
-    assert np.all(alpha[1] == 138)
-    assert np.all(alpha[2] == 138)
-    assert np.all(alpha[3] == 138)
+
+
+def test_red_band_magnitude_is_invariant_with_zoom_level():
+    input_data = _create_tcl_3band_data()
+    red = 0
+
+    zoomed_in = _apply_filter_with(input_data, zoom_level="12")
+    zoomed_out = _apply_filter_with(input_data, zoom_level="10")
+
+    assert np.all(zoomed_in[red] == zoomed_out[red])
+
+
+def test_green_band_gains_magnitude_as_zoom_increases():
+    input_data = _create_tcl_3band_data()
+    green = 1
+
+    zoomed_in = _apply_filter_with(input_data, zoom_level="12")
+    zoomed_out = _apply_filter_with(input_data, zoom_level="10")
+
+    assert np.all(zoomed_in[green] > zoomed_out[green])
+
+
+def test_blue_band_gains_magnitude_as_zoom_decreases():
+    input_data = _create_tcl_3band_data()
+    blue = 2
+
+    zoomed_in = _apply_filter_with(input_data, zoom_level="12")
+    zoomed_out = _apply_filter_with(input_data, zoom_level="10")
+
+    assert np.all(zoomed_in[blue] < zoomed_out[blue])
+
+
+def test_only_years_within_specified_range_are_visible():
+    input_data = _create_tcl_3band_data()
+
+    result = _apply_filter_with(input_data, start_year="2006", end_year="2014")
+    alpha = result[3]
+    year_2000, year_2005, year_2010, year_2015 = alpha
+    transparent = 0
+
+    assert np.all(year_2000 == transparent)
+    assert np.all(year_2005 == transparent)
+    assert np.all(year_2010 != transparent)
+    assert np.all(year_2015 == transparent)
+
+
+def test_multiple_years_are_visible():
+    input_data = _create_tcl_3band_data()
+
+    result = _apply_filter_with(input_data, start_year="2000", end_year="2020")
+    alpha = result[3]
+    year_2000, year_2005, year_2010, year_2015 = alpha
+    transparent = 0
+
+    assert np.all(year_2000 == transparent)
+    assert np.all(year_2005 != transparent)
+    assert np.all(year_2010 != transparent)
+    assert np.all(year_2015 != transparent)
+
+
+def test_transparency_increases_with_zoom_level():
+    input_data = _create_tcl_3band_data()
+
+    zoom_in = _apply_filter_with(input_data, zoom_level="12")
+    zoom_out = _apply_filter_with(input_data, zoom_level="10")
+    alpha = 3
+    year_2015 = 3
+
+    assert np.all(zoom_in[alpha][year_2015] < zoom_out[alpha][year_2015])
+
+
+def test_years_visible_when_no_start_or_end_year_is_given():
+    input_data = _create_tcl_3band_data()
+
+    result = _apply_filter_with(input_data, start_year=None, end_year=None)
+    alpha = result[3]
+    year_2000, year_2005, year_2010, year_2015 = alpha
+    transparent = 0
+
+    assert np.all(year_2000 == transparent)
+    assert np.all(year_2005 != transparent)
+    assert np.all(year_2010 != transparent)
+    assert np.all(year_2015 != transparent)
+
+
+def test_apply_annual_loss_filter_ignores_alpha_channel_already_in_data():
+    input_data_3band = _create_tcl_3band_data()
+    input_data_4band = _create_tcl_4band_data()
+
+    result_3band = _apply_filter_with(input_data_3band)
+    result_4band = _apply_filter_with(input_data_4band)
+
+    assert np.all(result_3band == result_4band)
