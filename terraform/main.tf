3c781211f59acbc887b98a01b58565aed1c22d72
Daniel Mannarino <daniel.mannarino@gmail.com>
2022-11-08 12:09:31 -0500

    Bump rasterio, pillow versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,8 +87,8 @@ module "lambda_raster_tiler" {
   source      = "./modules/lambda_raster_tiler"
   environment = var.environment
   lambda_layers = [
-    data.terraform_remote_state.lambda_layers.outputs.py38_pillow_920_arn,
-    data.terraform_remote_state.lambda_layers.outputs.py38_rasterio_132_arn,
+    data.terraform_remote_state.lambda_layers.outputs.py38_pillow_930_arn,
+    data.terraform_remote_state.lambda_layers.outputs.py38_rasterio_133_arn,
     data.terraform_remote_state.lambda_layers.outputs.py38_mercantile_121_arn
   ]
   log_level  = var.log_level
