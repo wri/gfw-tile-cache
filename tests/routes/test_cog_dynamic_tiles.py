6d8f6ddd5c24801cb1a21cb90f7540847314a695
Solomon Negusse <solomon.negusse@wri.org>
2024-07-03 15:59:27 +0300

    add integrated alerts test

--- tests/routes/test_cog_dynamic_tiles.py
+++ tests/routes/test_cog_dynamic_tiles.py
@@ -4,7 +4,7 @@ from ..conftest import parse_img
 
 
 @pytest.mark.asyncio
-async def test_cog_for_data_api_dataset(client):
+async def test_tile_for_data_api_dataset(client):
     response = client.get(
         "/cog/basic/tiles/WebMercatorQuad/8/87/48?dataset=umd_glad_landsat_alerts&version=v20210101"
     )
@@ -16,4 +16,15 @@ async def test_cog_for_data_api_dataset(client):
     assert meta["height"] == 256
 
 
-# TODO add integrated alerts test
+@pytest.mark.asyncio
+async def test_tiling_with_custom_rendering(client):
+    """Test Integrated Alerts Tile Rendering."""
+    response = client.get(
+        "/cog/custom/tiles/WebMercatorQuad/14/5305/8879?url=s3://gfw-data-lake-test&bands=default&bands=intensity&algorithm=integrated_alerts&return_mask=False&format=png"
+    )
+    response.status_code == 200
+
+    assert response.headers["content-type"] == "image/png"
+    meta = parse_img(response.content)
+    assert meta["width"] == 256
+    assert meta["height"] == 256
