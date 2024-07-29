bcbdebbd4c4683fad9f9dccb26958fa2a46b21d4
Daniel Mannarino <daniel.mannarino@gmail.com>
2024-07-29 14:02:58 -0400

    Use new numpy layer

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,6 +88,7 @@ module "lambda_raster_tiler" {
   source      = "./modules/lambda_raster_tiler"
   environment = var.environment
   lambda_layers = [
+    data.terraform_remote_state.lambda_layers.outputs.py310_numpy_1264_arn,
     data.terraform_remote_state.lambda_layers.outputs.py310_pillow_950_arn,
     data.terraform_remote_state.lambda_layers.outputs.py310_rasterio_138_arn,
     data.terraform_remote_state.lambda_layers.outputs.py310_mercantile_121_arn
