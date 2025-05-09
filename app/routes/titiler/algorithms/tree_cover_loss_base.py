368ba0eb140ef5ba17861f0e9a44fc4dbb72b482
manukala6 <manukala6@gmail.com>
2025-05-09 07:57:13 -0700

    GTC-3142 Adjust blocksize and simplify encodings

--- app/routes/titiler/algorithms/tree_cover_loss_base.py
+++ app/routes/titiler/algorithms/tree_cover_loss_base.py
@@ -67,19 +67,19 @@ class TreeCoverLossBase(BaseAlgorithm, ABC):
 
     def create_encoded_rgb(self):
         # Red = intensity
-        r = np.clip(self.intensity, 0, 255).astype("uint8")
+        r = self.intensity
 
         # Green = 0
         g = np.zeros_like(r, dtype="uint8")
 
         # Blue = year of loss (1–24)
-        b = np.where(self.mask, self.tree_cover_loss_data, 0).astype("uint8")
+        b = self.tree_cover_loss_data
 
         return np.stack([r, g, b], axis=0)
 
     def create_encoded_alpha(self):
         # Alpha = 255 where intensity > 0 and mask is True
-        alpha = np.where((self.intensity > 0) & self.mask, 255, 0).astype("uint8")
+        alpha = np.where((self.intensity > 0), 255, 0).astype("uint8")
 
         return alpha
 
