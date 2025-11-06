0062ae4753a78e0bb81354832dcd7d1baad40006
Dan Scales <dan.scales@gmail.com>
2025-11-06 14:58:46 -0800

    GTC-3357 Add forest filter to gfw_integrated_dist_alerts titiler endpoint.
    I moved most of the forest filter code down to the Alerts class, so it
    can be shared between DistAlerts and gfw_integrated_dist_alerts.py
    (which uses IntegratedAlerts). It could also be used by other Alerts
    subclasses.
    
    Dist alerts no longer needs a create_mask method, since it is fully
    handled by the Alerts create_mask method.
    
    Dockerfile change is just to add ssh-client to test docker, which makes
    it easier for me to test dev changes on Linux with production DB.

--- app/routes/titiler/gfw_integrated_dist_alerts.py
+++ app/routes/titiler/gfw_integrated_dist_alerts.py
@@ -2,10 +2,12 @@ import os
 from typing import Optional, Tuple
 
 from fastapi import APIRouter, Depends, Query, Response, Path
+from rio_tiler.io import COGReader
 from titiler.core.resources.enums import ImageType
 from titiler.core.utils import render_image
 
 from ...models.enumerators.titiler import IntegratedAlertConfidence, RenderType
+from ...settings.globals import GLOBALS
 from .. import DATE_REGEX, raster_xyz
 from .algorithms.integrated_alerts import IntegratedAlerts
 from .readers import AlertsReader
@@ -17,7 +19,7 @@ router = APIRouter()
 dataset = "gfw_integrated_dist_alerts"
 
 # We don't set a fixed set of versions that can be used, since we want to be able
-# to serve any new integrated_dist_alerts version created since this server started.
+# to serve any new gfw_integrated_dist_alerts version created since this server started.
 
 
 @router.get(
@@ -50,6 +52,21 @@ async def gfw_integrated_alerts_raster_tile(
         IntegratedAlertConfidence.low,
         description="Show alerts with at least this confidence level",
     ),
+    tree_cover_density_threshold: Optional[int] = Query(
+        None,
+        ge=0,
+        le=100,
+        description="Show alerts in pixels with tree cover density (in percent) greater than or equal to this threshold. `umd_tree_cover_density_2010` is used for this masking.",
+    ),
+    tree_cover_height_threshold: Optional[int] = Query(
+        None,
+        description="Show alerts in pixels with tree cover height (in meters) greater than or equal to this threshold. `umd_tree_cover_height_2020` dataset in the API is used for this masking.",
+    ),
+    tree_cover_loss_threshold: Optional[int] = Query(
+        None,
+        ge=2021,
+        description="""This filter is to be used in conjunction with `tree_cover_density_threshold` and `tree_cover_height_threshold` filters to detect only alerts in forests, by masking out pixels that have had tree cover loss prior to the alert.""",
+    ),
 ) -> Response:
     """GFW Integrated Disturbance Alerts raster tiles."""
 
@@ -59,12 +76,39 @@ async def gfw_integrated_alerts_raster_tile(
         tile_x, tile_y, zoom = xyz
         image_data = reader.tile(tile_x, tile_y, zoom, bands=bands)
 
-    processed_image = IntegratedAlerts(
+    int_dist_alert = IntegratedAlerts(
         start_date=start_date,
         end_date=end_date,
         render_type=render_type,
         alert_confidence=alert_confidence,
-    )(image_data)
+        tree_cover_density_mask=tree_cover_density_threshold,
+        tree_cover_height_mask=tree_cover_height_threshold,
+        tree_cover_loss_mask=tree_cover_loss_threshold,
+    )
+
+    filter_datasets = GLOBALS.dist_alerts_forest_filters
+    if tree_cover_density_threshold:
+        filter_dataset = filter_datasets["tree_cover_density"]
+        with COGReader(
+            f"s3://{DATA_LAKE_BUCKET}/{filter_dataset['dataset']}/{filter_dataset['version']}/raster/epsg-4326/cog/default.tif"
+        ) as reader:
+            int_dist_alert.tree_cover_density_data = reader.tile(tile_x, tile_y, zoom)
+
+    if tree_cover_height_threshold:
+        filter_dataset = filter_datasets["tree_cover_height"]
+        with COGReader(
+            f"s3://{DATA_LAKE_BUCKET}/{filter_dataset['dataset']}/{filter_dataset['version']}/raster/epsg-4326/cog/default.tif"
+        ) as reader:
+            int_dist_alert.tree_cover_height_data = reader.tile(tile_x, tile_y, zoom)
+
+    if tree_cover_loss_threshold:
+        filter_dataset = filter_datasets["tree_cover_loss"]
+        with COGReader(
+            f"s3://{DATA_LAKE_BUCKET}/{filter_dataset['dataset']}/{filter_dataset['version']}/raster/epsg-4326/cog/default.tif"
+        ) as reader:
+            int_dist_alert.tree_cover_loss_data = reader.tile(tile_x, tile_y, zoom)
+
+    processed_image = int_dist_alert(image_data)
 
     content, media_type = render_image(
         processed_image,
