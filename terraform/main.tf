1f2ae8981b2de574a912550bc3e503a7505916a8
Daniel Mannarino <daniel.mannarino@gmail.com>
2025-11-25 11:07:04 -0500

    Fix references to lambda layers SSM params

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,10 +87,10 @@ module "lambda_raster_tiler" {
   source      = "./modules/lambda_raster_tiler"
   environment = var.environment
   lambda_layers = [
-    local.core.lambda_layers.py310_numpy_1264_arn,
-    local.core.lambda_layers.py310_pillow_950_arn,
-    local.core.lambda_layers.py310_rasterio_no_numpy_arn,
-    local.core.lambda_layers.py310_mercantile_121_arn
+    local.lambda_layers.py310_numpy_1264_arn,
+    local.lambda_layers.py310_pillow_950_arn,
+    local.lambda_layers.py310_rasterio_no_numpy_arn,
+    local.lambda_layers.py310_mercantile_121_arn
   ]
   lambda_runtime        = var.lambda_runtime
   log_level             = var.log_level
