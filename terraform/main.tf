c5bab70f2ba26f0de1836fd2e26164c4f9f740e6
Daniel Mannarino <daniel.mannarino@gmail.com>
2025-11-26 17:07:28 -0500

    Fix ref to numpy layer

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,7 +87,7 @@ module "lambda_raster_tiler" {
   source      = "./modules/lambda_raster_tiler"
   environment = var.environment
   lambda_layers = [
-    local.lambda_layers.py310_numpy_1264_arn,
+    local.lambda_layers.py310_numpy_arn,
     local.lambda_layers.py310_pillow_950_arn,
     local.lambda_layers.py310_rasterio_no_numpy_arn,
     local.lambda_layers.py310_mercantile_121_arn
