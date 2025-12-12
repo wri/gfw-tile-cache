ff6bb7e37ebba6fe7276525de6fdab39c97fab74
Daniel Mannarino <daniel.mannarino@gmail.com>
2025-12-12 16:29:04 -0500

    Make code compatible with Python 3.11

--- app/models/enumerators/titiler.py
+++ app/models/enumerators/titiler.py
@@ -1,35 +1,41 @@
-from enum import Enum
+# After upgrading to Python 3.11, this can become just
+# from enum import StrEnum
+try:
+    from enum import StrEnum
+except ImportError:
+    from enum import Enum
+    class StrEnum(str, Enum):
+        pass
 
 
-class AlertConfidence(str, Enum):
+class AlertConfidence(StrEnum):
     low = "low"
     high = "high"
 
 
-class IntegratedAlertConfidence(str, Enum):
+class IntegratedAlertConfidence(StrEnum):
     low = "low"
     high = "high"
     highest = "highest"
 
 
-class RenderType(str, Enum):
+class RenderType(StrEnum):
     true_color = "true_color"
     encoded = "encoded"
 
 
-class TreeCoverDensityThreshold(str, Enum):
+class TreeCoverDensityThreshold(StrEnum):
+    tcd_30 = "30"
+    tcd_50 = "50"
+    tcd_75 = "75"
 
-    tcd_30 = 30
-    tcd_50 = 50
-    tcd_75 = 75
 
-
-class TCLTreeCoverDensityThreshold(str, Enum):
-    tcd_10 = 10
-    tcd_15 = 15
-    tcd_20 = 20
-    tcd_25 = 25
-    tcd_30 = 30
-    tcd_50 = 50
-    tcd_75 = 75
-    tcd_90 = 90
+class TCLTreeCoverDensityThreshold(StrEnum):
+    tcd_10 = "10"
+    tcd_15 = "15"
+    tcd_20 = "20"
+    tcd_25 = "25"
+    tcd_30 = "30"
+    tcd_50 = "50"
+    tcd_75 = "75"
+    tcd_90 = "90"
