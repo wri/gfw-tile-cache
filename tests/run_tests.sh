0178fb69bf3eababaceffd87fd9b31329ddd63ef
Daniel Mannarino <daniel.mannarino@gmail.com>
2026-01-06 10:18:16 -0500

    Merge uv-ification, Python upgrade to production (#253)
    * Convert to uv from pipenv
    * Switch to ubuntu based image
    * Bump PostgreSQL version to match deployed
    * Bump Python to 3.11
    
    This reverts commit 07632e7cc385dabbeed5afafd241035e8e836ec6.
    
    ---------
    
    Co-authored-by: Dan Scales <dan.scales@gmail.com>

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,4 +7,3 @@ terraform init && terraform plan && terraform apply -auto-approve
 popd
 
 wait_for_postgres.sh pytest --cov-report term --cov-report xml:/app/tests/cobertura.xml --cov=app --cov=lambdas "$@"
-
