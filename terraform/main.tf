f1f8194a8724f9525c4a9fd2d21fd9db674c3422
Daniel Mannarino <daniel.mannarino@gmail.com>
2026-01-21 11:18:15 -0500

    Add values to SSM needed by Data API (#258)

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ locals {
 
 # Docker file for FastAPI app
 module "container_registry" {
-  source       = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/container_registry?ref=v0.4.2.9"
+  source       = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/container_registry?ref=v0.4.2.12"
   image_name   = lower("${local.project}${local.name_suffix}")
   root_dir     = "../${path.root}"
   tag          = local.container_tag
@@ -26,7 +26,7 @@ module "container_registry" {
 }
 
 module "orchestration" {
-  source                       = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/fargate_autoscaling?ref=v0.4.2.9"
+  source                       = "git::https://github.com/wri/gfw-terraform-modules.git//terraform/modules/fargate_autoscaling?ref=v0.4.2.12"
   project                      = local.project
   name_suffix                  = local.name_suffix
   tags                         = local.tags
@@ -110,12 +110,15 @@ module "ssm" {
   environment = var.environment
   namespace   = "gfw-tile-cache"
   contract = {
-    tile_cache_bucket        = module.storage.tiles_bucket_name
-    tile_cache_bucket_arn    = module.storage.tiles_bucket_arn
-    tile_cache_cloudfront_id = module.content_delivery_network.cloudfront_distribution_id
-    tile_cache_url           = local.tile_cache_url
-    tile_cache_cluster       = module.orchestration.ecs_cluster_name
-    tile_cache_service       = module.orchestration.ecs_service_name
+    cloudfront_invalidation_policy_arn = module.content_delivery_network.cloudfront_invalidation_policy_arn
+    ecs_update_service_policy_arn      = module.orchestration.ecs_update_service_policy_arn
+    tile_cache_bucket                  = module.storage.tiles_bucket_name
+    tile_cache_bucket_arn              = module.storage.tiles_bucket_arn
+    tile_cache_bucket_write_policy_arn = module.storage.s3_write_tiles_arn
+    tile_cache_cloudfront_id           = module.content_delivery_network.cloudfront_distribution_id
+    tile_cache_url                     = local.tile_cache_url
+    tile_cache_cluster                 = module.orchestration.ecs_cluster_name
+    tile_cache_service                 = module.orchestration.ecs_service_name
   }
   lists = {}
   strings = {}
